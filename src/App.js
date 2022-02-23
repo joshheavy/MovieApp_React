7f8a5856cbf437981f045b75a21c47514e2cbc40
Joshheavy <waruriijoshua21@gmail.com>
2022-02-23 05:52:03 -0500

    movie app with framer motion package

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import React from "react";
 import './App.css';
+import Movie from "./Movie";
+import Filter from './Filter';
+import { motion, AnimatePresence } from 'framer-motion';
 
 function App() {
+  const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+  const fetchPopular = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=a8d2809ffb252ccf808f1d3e5c5cd3ba&language=en-US&page=1"
+    );
+    // turn the data into a json objects thats easy to work with
+    const movies = await data.json();
+    //store the movies so use useState
+    setPopular(movies.results);
+    setFiltered(movies.results);
+    console.log(movies);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Filter
+        popular={popular}
+        setFiltered={setFiltered}
+        activeGenre={activeGenre}
+        setActiveGenre={setActiveGenre}
+      />
+      <motion.div layout className="popular-movies">
+        <AnimatePresence>
+          {filtered.map((movie) => {
+            return <Movie key={movie.id} movie={movie} />;
+          })}
+        </AnimatePresence>
+      </motion.div>
     </div>
   );
-}
+};
+
+  // https://api.themoviedb.org/3/movie/550?api_key=a8d2809ffb252ccf808f1d3e5c5cd3ba
+  
 
 export default App;
